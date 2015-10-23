ca8fe332093833ee84557b6045e2b51b78382675
bfrager <btfrager@gmail.com>
2015-10-23 12:00:35 -0700

    added pictures of whales

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+@@database = [
+  {url: "https://www.rottnestfastferries.com.au/wp-content/uploads/2013/05/whale-watch.jpg", title: "Flipping Whale"},
+  {url: "http://adventuremaui.com/images/435/whales3.jpg", title: "Mommy and baby"},
+  {url: "http://images.clipartpanda.com/whale-clipart-black-and-white-whale_copy_black_white_line_art_christmas_xmas_stuffed_animal-3333px.png", title: "Cartoon Whale"}
+ ]
+  
 end
