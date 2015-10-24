cae4829ce7ff9b841b0cc63f44dc2f5ca0e16e3b
bfrager <btfrager@gmail.com>
2015-10-23 17:04:03 -0700

    added delete button and functionality

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,4 +6,9 @@ class PhotosController < ApplicationController
   def show
     @photo = @@database[params[:id].to_i]
   end
+
+  def destroy
+    @@database.delete_at(params[:id].to_i)
+    redirect_to('/')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
 
   # Example of regular route:
      get '/photos/:id' => 'photos#show'
+     delete '/photos/:id' => 'photos#destroy'
+
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
